a9ffa4c7aac1a6593dc5773636dd129412012a82
Twan van Noorloos <twan.v.n@outlook.com>
2021-12-09 11:11:19 +0100

    Implemented methods in interface DataStorage
    Methods added to the interface DataStorage and the AppDatabasemanager implements these methods, methods have no body

--- app/src/main/java/nl/ags/picum/dataStorage/managing/AppDatabaseManager.java
+++ app/src/main/java/nl/ags/picum/dataStorage/managing/AppDatabaseManager.java
@@ -1,4 +1,58 @@
 package nl.ags.picum.dataStorage.managing;
 
+import java.util.List;
+
+import nl.ags.picum.dataStorage.roomData.AppDatabase;
+import nl.ags.picum.dataStorage.roomData.Route;
+import nl.ags.picum.dataStorage.roomData.Waypoint;
+
 public class AppDatabaseManager implements DataStorage {
+    private static AppDatabase database;
+
+    public AppDatabaseManager() {
+        
+    }
+
+    public static AppDatabase getInstance() {
+        if (database == null) {
+            database = new AppDatabase();
+        }
+
+        return database;
+    }
+
+    @Override
+    public List<Route> getRoutes() {
+        return null;
+    }
+
+    @Override
+    public Route getCurrentRoute() {
+        return null;
+    }
+
+    @Override
+    public void setCurrentRoute(Route route) {
+
+    }
+
+    @Override
+    public List<Waypoint> getHistory(Route route) {
+        return null;
+    }
+
+    @Override
+    public void setHistory(Route route, List<Waypoint> waypoints) {
+
+    }
+
+    @Override
+    public void appendHistory(Route route, Waypoint waypoint) {
+
+    }
+
+    @Override
+    public void clearHistory(Route route) {
+
+    }
 }
--- app/src/main/java/nl/ags/picum/dataStorage/managing/DataStorage.java
+++ app/src/main/java/nl/ags/picum/dataStorage/managing/DataStorage.java
@@ -1,4 +1,17 @@
 package nl.ags.picum.dataStorage.managing;
 
+import java.util.List;
+
+import nl.ags.picum.dataStorage.roomData.Route;
+import nl.ags.picum.dataStorage.roomData.Waypoint;
+
 public interface DataStorage {
+    List<Route> getRoutes();
+    Route getCurrentRoute();
+    void setCurrentRoute(Route route);
+
+    List<Waypoint> getHistory(Route route);
+    void setHistory(Route route, List<Waypoint> waypoints);
+    void appendHistory(Route route, Waypoint waypoint);
+    void clearHistory(Route route);
 }
