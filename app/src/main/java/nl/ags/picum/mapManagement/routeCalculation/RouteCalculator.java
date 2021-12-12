bb0fae87d1e2db594a364ab20d89e99a335098b4
Jesse <jm.krijgsman@Student.avans.nl>
2021-12-12 17:20:34 +0100

    Created OpenRouteURLs class

--- app/src/main/java/nl/ags/picum/mapManagement/routeCalculation/RouteCalculator.java
+++ app/src/main/java/nl/ags/picum/mapManagement/routeCalculation/RouteCalculator.java
@@ -1,9 +1,25 @@
 package nl.ags.picum.mapManagement.routeCalculation;
 
-//Todo remove when implemented
-interface Route {}
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import nl.ags.picum.dataStorage.roomData.Waypoint;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RouteCalculator {
+    public static String LOG_TAG = RouteCalculator.class.getName();
 
     private RouteCalculatorListener listener;
 
@@ -11,7 +27,10 @@ public class RouteCalculator {
         this.listener = listener;
     }
 
-    public void calculate(Route route) {
-        
+    public void calculate(List<Waypoint> waypointList) {
+
+
     }
+
+
 }
