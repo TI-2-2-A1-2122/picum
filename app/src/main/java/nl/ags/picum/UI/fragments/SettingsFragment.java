4a9bbe2bb61362793e09034c8bb30f8ea7855d04
Yassin Diriye <58508237+yassindiriye@users.noreply.github.com>
2021-12-13 10:35:20 +0100

    images added in fragment

---
+++ app/src/main/java/nl/ags/picum/UI/fragments/SettingsFragment.java
@@ -0,0 +1,40 @@
+package nl.ags.picum.UI.fragments;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Locale;
+
+import nl.ags.picum.R;
+
+public class SettingsFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+//
+//
+//        Locale locale = new Locale("en");
+//        Locale.setDefault(locale);
+//        Configuration configuration = new Configuration();
+//        configuration.locale = locale;
+//        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+//        System.out.println("hey schemzy");
+//        setContentView(R.layout.activity_main);
+    }
+}
