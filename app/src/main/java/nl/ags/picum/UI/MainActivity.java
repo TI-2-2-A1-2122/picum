33f90eb30356916c9079c21ca31baa68bf381693
Ewoodss <70382659+Ewoodss@users.noreply.github.com>
2021-12-13 15:27:42 +0100

    MainActivity
    Added mapactivity to manifest
    cleaned up mainactivity

--- app/src/main/java/nl/ags/picum/UI/MainActivity.java
+++ app/src/main/java/nl/ags/picum/UI/MainActivity.java
@@ -1,42 +1,33 @@
 package nl.ags.picum.UI;
 
+import android.Manifest;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
-import android.graphics.Point;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import nl.ags.picum.R;
+import nl.ags.picum.UI.Util.RouteAdapter;
 import nl.ags.picum.UI.fragments.RouteDetailsFragment;
 import nl.ags.picum.dataStorage.managing.AppDatabaseManager;
 import nl.ags.picum.dataStorage.roomData.Route;
-import nl.ags.picum.UI.Util.RouteAdapter;
-import nl.ags.picum.location.gps.Location;
-import nl.ags.picum.location.gps.LocationObserver;
-import nl.ags.picum.dataStorage.roomData.Waypoint;
-import nl.ags.picum.mapManagement.routeCalculation.RouteCalculator;
-import nl.ags.picum.mapManagement.routeCalculation.RouteCalculatorListener;
 import nl.ags.picum.permission.PermissionManager;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Route> routes = new ArrayList<>();
+    private final List<Route> routes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PermissionManager permissionManager = new PermissionManager();
-        permissionManager.requestPermissions(new String[] {
+        permissionManager.requestPermissions(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         }, this, getApplicationContext());
@@ -51,48 +42,18 @@ public class MainActivity extends AppCompatActivity {
             this.routes.clear();
 
             List<Route> tempList = manager.getRoutes();
+
             this.routes.addAll(tempList);
 
             recyclerView.getAdapter().notifyDataSetChanged();
         }).start();
 
-        /*
-        //TODO change code for implementation
-        List<Route> routes = new ArrayList<>();
-        routes.add(new Route("History", "Dit is een kilomter om te lopen", 0, false));
-        routes.add(new Route("Geen history", "Dit is geen historische kilometer", 0, false));
-
-         */
-//
-//        RecyclerView recyclerView = findViewById(R.id.main_routes_recyclerview);
-//        recyclerView.setAdapter(new RouteAdapter(routes, this));
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-//        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
-        
-
-        Waypoint w1 = new Waypoint(1,false, 51.740484f, 4.544803f);
-        Waypoint w2 = new Waypoint(2,false, 51.771082f, 4.614198f);
-
-        List<Waypoint> waypointList = new ArrayList<>();
-        waypointList.add(w1);
-        waypointList.add(w2);
-
-        RouteCalculator calculator = new RouteCalculator(new RouteCalculatorListener() {
-            @Override
-            public void onRoutePointsCalculated(List<Point> points) {
-                Log.d("TESTSSSSSSSSSSSS", points.toString());
-            }
-        });
-
-        calculator.calculate(waypointList);
     }
 
     //TODO change to nonstatic
-    public void showDetailsFragment(Route selectedRoute){
+    public void showDetailsFragment(Route selectedRoute) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         new RouteDetailsFragment(selectedRoute).show(fragmentManager, "Dialog-popup");
     }
-
-
 
 }
