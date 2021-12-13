05330f8696d83b407e869ef4d72d8ae3a692c105
Yassin Diriye <58508237+yassindiriye@users.noreply.github.com>
2021-12-13 11:01:15 +0100

    added translation an language switching

--- app/src/main/java/nl/ags/picum/UI/fragments/SettingsFragment.java
+++ app/src/main/java/nl/ags/picum/UI/fragments/SettingsFragment.java
@@ -9,15 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.Locale;
 
 import nl.ags.picum.R;
 
 public class SettingsFragment extends DialogFragment {
+    private Button englishButton;
+    private Button dutchButtom;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        englishButton = getActivity().findViewById(R.id.englishButtonIMG);
+        dutchButtom = getActivity().findViewById(R.id.dutchButtonIMG);
+
+
         super.onCreate(savedInstanceState);
     }
 
@@ -26,6 +34,9 @@ public class SettingsFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+
 
 //
 //
@@ -36,5 +47,25 @@ public class SettingsFragment extends DialogFragment {
 //        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
 //        System.out.println("hey schemzy");
 //        setContentView(R.layout.activity_main);
+    }
+
+    public void toEnglish(View view) {
+        Locale locale = new Locale("en");
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getActivity().getBaseContext().getResources().updateConfiguration(configuration, getActivity().getBaseContext().getResources().getDisplayMetrics());
+        System.out.println("hey schemzy");
+        getActivity().setContentView(R.layout.activity_main);
+    }
+
+    public void toDutch(View view) {
+        Locale locale = new Locale("nl");
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getActivity().getBaseContext().getResources().updateConfiguration(configuration, getActivity().getBaseContext().getResources().getDisplayMetrics());
+        System.out.println("hey schemzy");
+        getActivity().setContentView(R.layout.activity_main);
     }
 }
