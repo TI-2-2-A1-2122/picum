696256093a1fba815536f42e1e66bac05ed3d9b4
Luca <Luca.1999@hotmail.com>
2021-12-16 11:46:38 +0100

    added sightsee popup fragment

---
+++ app/src/main/java/nl/ags/picum/UI/fragments/SightDetailsPopupFragment.java
@@ -0,0 +1,27 @@
+package nl.ags.picum.UI.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nl.ags.picum.R;
+
+public class SightDetailsPopupFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sight_details_popup, container, false);
+    }
+}
