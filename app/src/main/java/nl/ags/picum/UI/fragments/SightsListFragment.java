d13ee20473fdaedd84a270bf1254ba4d8513a940
Luca <Luca.1999@hotmail.com>
2021-12-16 14:30:10 +0100

    Little changes

---
+++ app/src/main/java/nl/ags/picum/UI/fragments/SightsListFragment.java
@@ -0,0 +1,28 @@
+package nl.ags.picum.UI.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nl.ags.picum.R;
+
+public class SightsListFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sights_list, container, false);
+    }
+}
